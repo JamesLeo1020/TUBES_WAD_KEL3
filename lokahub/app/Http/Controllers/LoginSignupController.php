1aea788a11956baaddef3abe7e1f89be80bf0240
james hestu satria <jamesleo164@gmail.com>
2024-12-21 20:44:58 +0700

    CRM

---
+++ lokahub/app/Http/Controllers/LoginSignupController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LoginSignup;
+use Illuminate\Http\Request;
+
+class LoginSignupController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LoginSignup $loginSignup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LoginSignup $loginSignup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LoginSignup $loginSignup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LoginSignup $loginSignup)
+    {
+        //
+    }
+}
